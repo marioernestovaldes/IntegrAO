9169ef7dc8f8c4e5ef4b321278f68fe3dce34b51
Mario E Valdes-Tresanco <marioe911116@gmail.com>
2025-05-04 20:14:39 -0600

    reformat IntegrAO_unsupervised.py

--- integrao/IntegrAO_unsupervised.py
+++ integrao/IntegrAO_unsupervised.py
@@ -1,7 +1,4 @@
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-import numpy as np
 
 from torch_geometric.nn import GraphSAGE
 
@@ -9,7 +6,7 @@ from torch_geometric.nn import GraphSAGE
 class IntegrAO(nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_layers=2):
         super(IntegrAO, self).__init__()
-        self.in_channels = in_channels # this is an array since we have multiple domains
+        self.in_channels = in_channels  # this is an array since we have multiple domains
         self.hidden_channels = hidden_channels
         self.output_dim = out_channels
         self.num_layers = num_layers
@@ -19,13 +16,13 @@ class IntegrAO(nn.Module):
 
         for i in range(num):
             model_sage = GraphSAGE(
-                in_channels=self.in_channels[i], 
-                hidden_channels=self.hidden_channels, 
-                num_layers=self.num_layers, 
+                in_channels=self.in_channels[i],
+                hidden_channels=self.hidden_channels,
+                num_layers=self.num_layers,
                 out_channels=self.output_dim,
                 project=False,
-)
-            
+            )
+
             feature.append(model_sage)
 
         self.feature = nn.ModuleList(feature)
@@ -43,5 +40,5 @@ class IntegrAO(nn.Module):
             z = self.feature[domain](x_dict[domain], edge_index_dict[domain])
             z = self.feature_show(z)
             z_all[domain] = z
-        
+
         return z_all
