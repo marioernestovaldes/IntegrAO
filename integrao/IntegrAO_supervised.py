be26b04b6cc272ea9d4a821fe4c409584e4998e1
Mario E Valdes-Tresanco <marioe911116@gmail.com>
2025-05-04 20:14:26 -0600

    reformat IntegrAO_supervised.py

--- integrao/IntegrAO_supervised.py
+++ integrao/IntegrAO_supervised.py
@@ -1,33 +1,30 @@
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-import numpy as np
 import torch
-
+import torch.nn as nn
 from torch_geometric.nn import GraphSAGE
 
 
 class IntegrAO(nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers=2, pred_n_layer=2, pred_act="softplus", num_classes=None):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers=2, pred_n_layer=2, pred_act="softplus",
+                 num_classes=None):
         super(IntegrAO, self).__init__()
-        self.in_channels = in_channels # this is an array since we have multiple domains
+        self.in_channels = in_channels  # this is an array since we have multiple domains
         self.hidden_channels = hidden_channels
         self.output_dim = out_channels
         self.num_layers = num_layers
         self.num_classes = num_classes
-        self.pred_n_layer=2,
-        self.pred_act="softplus",
+        self.pred_n_layer = 2,
+        self.pred_act = "softplus",
 
         num = len(in_channels)
         feature = []
         for i in range(num):
             model_sage = GraphSAGE(
-                in_channels=self.in_channels[i], 
-                hidden_channels=self.hidden_channels, 
-                num_layers=self.num_layers, 
+                in_channels=self.in_channels[i],
+                hidden_channels=self.hidden_channels,
+                num_layers=self.num_layers,
                 out_channels=self.output_dim,
-                project=False,)
-            
+                project=False, )
+
             feature.append(model_sage)
 
         self.feature = nn.ModuleList(feature)
@@ -40,7 +37,7 @@ class IntegrAO(nn.Module):
         )
 
         self.pred_head = nn.Sequential(
-            nn.Linear(self.output_dim, self.output_dim // 2 ),
+            nn.Linear(self.output_dim, self.output_dim // 2),
             nn.BatchNorm1d(self.output_dim // 2),
             nn.LeakyReLU(0.1, True),
             # nn.Softplus(),
@@ -49,10 +46,8 @@ class IntegrAO(nn.Module):
 
         print(self)
 
-
     def get_sample_ids_for_domain(self, domain):
         return self.sample_ids[domain]
-
 
     def forward(self, x_dict, edge_index_dict, domain_sample_ids):
         z_all = {}
@@ -81,7 +76,7 @@ class IntegrAO(nn.Module):
             # Stack all vectors along a new dimension and calculate the mean
             z_avg[sample_id] = torch.stack(vectors).mean(dim=0)
 
-        sorted_list = z_avg.items() # sorted(z_avg.items())
+        sorted_list = z_avg.items()  # sorted(z_avg.items())
         z_avg_list = [z_avg for _, z_avg in sorted_list]
         z_id_list = [z_id for z_id, _ in sorted_list]
         z_avg_tensor = torch.stack(z_avg_list)
